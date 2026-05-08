7f0a33ef8516867b1b01e50b051aa2f61e517cef
proxy19c <184772939+proxy19C@users.noreply.github.com>
2026-05-08 18:07:40 +0400

    style: prettier formatting

--- src/pages/settings/model/use-filters.ts
+++ src/pages/settings/model/use-filters.ts
@@ -66,7 +66,7 @@ export const useFilters = () => {
 		hsrSearchValue,
 		hsrSearchDebounced,
 		activeFilters,
-        displayedCharacters,
+		displayedCharacters,
 		isFilterActive,
 		enableFilter,
 		disableFilter,
