a0fb4c3fa062e1d7a2954197a9334345aa501d8c
proxy19c <184772939+proxy19C@users.noreply.github.com>
2026-05-08 17:55:04 +0400

    feat: keep locale in localStorage

--- src/app/providers/i18n/i18n.ts
+++ src/app/providers/i18n/i18n.ts
@@ -3,10 +3,19 @@ import { createI18n } from "vue-i18n";
 import en from "./locales/en.locale.json";
 import jaJP from "./locales/ja.locale.json";
 
+const getLocale = () => {
+	const savedLocale = localStorage.getItem("i18n:locale");
+	const browserLocale = navigator.language;
+
+	return savedLocale ?? browserLocale ?? "en";
+};
+
 export const i18n = createI18n({
 	legacy: false,
 	availableLocales: ["en", "ja-JP"],
 	fallbackLocale: "en",
+	locale: getLocale(),
+	
 	messages: {
 		en,
 		"ja-JP": jaJP,
